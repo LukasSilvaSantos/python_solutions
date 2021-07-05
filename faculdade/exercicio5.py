e60484b2eaf91fc4f5dd048957577a6b12ece106
Lucas da Silva <lucassilva.dev.trabalho@gmail.com>
2021-07-05 14:55:18 -0300

    exercicio while

--- faculdade/exercicio5.py
+++ faculdade/exercicio5.py
@@ -1,8 +1,8 @@
 print('CAlCULANDO... \n + = ADIÇÃO \n - = Subtração \n * = Multiplicação \n / = Divisão.')
-operação = input('qual operação deseja fazer?: ')
-numero1 = float(input('digite um valor: '))
-numero2 = float(input('digite um valor: '))
+operação = input('qual operação deseja fazer?: ').lower()
 while (operação != 'sair'):
+    numero1 = float(input('digite um valor: '))
+    numero2 = float(input('digite um valor: '))
     if (operação == '+'):
         soma = numero1 + numero2
         print(f'A soma de {numero1} + {numero2} é {soma}.')
@@ -16,7 +16,8 @@ while (operação != 'sair'):
         soma = numero1 / numero2
         print(f'A divisão de {numero1} / {numero2} é {soma}.')
     print('CAlCULANDO... \n + = ADIÇÃO \n - = Subtração \n * = Multiplicação \n / = Divisão.')
-    operação = input('qual operação deseja fazer?: ')
-    numero1 = float(input('digite um valor: '))
-    numero2 = float(input('digite um valor: '))
+    operação = input('qual operação deseja fazer?: ').lower()
+    
 print('Encerrando o programa...')
+input()
+
