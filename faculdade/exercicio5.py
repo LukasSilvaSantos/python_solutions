494b054026d7a53ede05f770a21c6f79b6843460
Lucas da Silva <lucassilva.dev.trabalho@gmail.com>
2021-07-05 14:25:52 -0300

    exercicio while

--- faculdade/exercicio5.py
+++ faculdade/exercicio5.py
@@ -1,18 +1,22 @@
-numero1 = int(input('escreva um numero inteiro: '))
-numero2 = int(input('escreva um numero inteiro: '))
-operação = input('Escolha uma dessas operações: (+), (-), (*), (/): ')
-if operação == '+':
-    calculo = (numero1 + numero2)
-    print (f'O resultado da operação foi {calculo}')
-else:
-    if operação == '-':
-        calculo = (numero1 - numero2)
-        print (f'O resultado da operação foi {calculo}')
-    else:
-        if operação == '*':
-            calculo = (numero1 * numero2)
-            print (f'O resultado da operação foi {calculo}')
-        else:
-            if operação == '/':
-                calculo = (numero1 / numero2)
-                print (f'O resultado da operação foi {calculo}')        
+print('CAlCULANDO... \n + = ADIÇÃO \n - = Subtração \n * = Multiplicação \n / = Divisão.')
+operação = input('qual operação deseja fazer?: ')
+numero1 = float(input('digite um valor: '))
+numero2 = float(input('digite um valor: '))
+while (operação != 'sair'):
+    if (operação == '+'):
+        soma = numero1 + numero2
+        print(f'A soma de {numero1} + {numero2} é {soma}.')
+    elif (operação == '-'):
+        soma = numero1 - numero2
+        print(f'A subtração de {numero1} - {numero2} é {soma}.')
+    elif (operação == '*'):
+        soma = numero1 * numero2
+        print(f'A multiplicação de {numero1} * {numero2} é {soma}.')
+    elif (operação == '/'):
+        soma = numero1 / numero2
+        print(f'A divisão de {numero1} / {numero2} é {soma}.')
+    print('CAlCULANDO... \n + = ADIÇÃO \n - = Subtração \n * = Multiplicação \n / = Divisão.')
+    operação = input('qual operação deseja fazer?: ')
+    numero1 = float(input('digite um valor: '))
+    numero2 = float(input('digite um valor: '))
+print('Encerrando o programa...')
