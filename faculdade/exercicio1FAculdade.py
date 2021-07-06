78b9bbe5020e071bd6caff8bcf3c73d7ccb976bc
Lucas da Silva <lucassilva.dev.trabalho@gmail.com>
2021-07-06 10:57:19 -0300

    exercicio 5

--- faculdade/exercicio1FAculdade.py
+++ faculdade/exercicio1FAculdade.py
@@ -1,5 +1,9 @@
-preçodoProduto = float(input('qual o preço do produto?: '))
-desconto = float(input('qual o valor do desconto?: '))
-porcentagem = preçodoProduto * (desconto / 100)
-valorFinal = preçodoProduto - porcentagem
-print(f'o valor do produto com o desconto aplicado será de R${valorFinal}.')
+print(' vamos calcular o desconto nos produtos. \n digite na tela o valor do produto é o valor do desconto. \n OBS: (Não coloque nenhum caracter no valor do produto ou do desconto, só coloque os números.)')
+preçodoProduto = input('qual o preço do produto?: ')
+desconto = input('qual o valor do desconto?: ')
+if (preçodoProduto.isnumeric() and desconto.isnumeric() == True):
+    porcentagem = float(preçodoProduto) * float(desconto) / 100
+    valorFinal = float(preçodoProduto) - porcentagem
+    print(f'o valor do produto com o desconto aplicado será de R${valorFinal:.2f}')
+
+    
