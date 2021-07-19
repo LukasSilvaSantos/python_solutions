9c56bccf3957056be7256220e6d03b92872401ec
Lucas da Silva <lucassilva.dev.trabalho@gmail.com>
2021-07-19 19:40:17 -0300

    exercicio3

---
+++ curso_em_video/estruturas de repetição/for/exercicio4.py
@@ -0,0 +1,3 @@
+for i in range(0, 10):
+    numero = int(input('digite um numero: '))
+    print(f'{numero}*1={numero*1}\n{numero}*2={numero*2}\n{numero}*3={numero*3}\n{numero}*4={numero*4}\n{numero}*5={numero*5}\n{numero}*6={numero*6}\n{numero}*7={numero*7}\n{numero}*8={numero*8}\n{numero}*9={numero*9}\n{numero}*10={numero*10}')
